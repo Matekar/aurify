c28230134c643c1e4268246b221254b019a6871e
Marek Dzierżawa <92123573+marekdzierzawa@users.noreply.github.com>
2024-05-20 01:26:29 +0200

    magic powder and oceanic core

---
+++ src/main/java/pl/edu/agh/fis/is/io/aurify/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.marek.aurifymod.item;
+
+import net.marek.aurifymod.AurifyMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AurifyMod.MOD_ID);
+
+    public static final RegistryObject<Item> MAGICDUST = ITEMS.register("magic_dust",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> OCEANICCORE = ITEMS.register("oceanic_core",
+            () -> new Item(new Item.Properties()));
+    public static void register(IEventBus eventbus){
+        ITEMS.register(eventbus);
+    }
+}
