c3e3ad33b59bc63c9f0fe9e77774b69e1329e7b1
Globus712 <kkrzysztofek@onet.pl>
2024-05-28 21:10:51 +0200

    Dodanie brakujących plików v0.3

---
+++ src/main/java/pl/edu/agh/fis/is/io/aurify/datagen/DataGenerators.java
@@ -0,0 +1,19 @@
+package pl.edu.agh.fis.is.io.aurify.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+
+import java.util.concurrent.CompletableFuture;
+
+public class DataGenerators {
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+        generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
+    }
+}
