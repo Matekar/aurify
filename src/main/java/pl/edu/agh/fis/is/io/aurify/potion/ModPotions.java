2b57003e24f9f39936d044a6bb6f962bd291d57f
Matekar <kiewskimarcin@gmail.com>
2024-06-15 03:01:29 +0200

    Changed all potions to comfort with new API

--- src/main/java/pl/edu/agh/fis/is/io/aurify/potion/ModPotions.java
+++ src/main/java/pl/edu/agh/fis/is/io/aurify/potion/ModPotions.java
@@ -4,6 +4,7 @@ import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.alchemy.Potion;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -17,11 +18,9 @@ public class ModPotions {
     public static final DeferredRegister<MobEffect> EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, AurifyMod.MODID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AurifyMod.MODID);
 
-    public static final RegistryObject<Potion> BLINDNESS_POTION = POTIONS.register("blindness_potion",
-            () -> new Potion(new MobEffectInstance(MobEffects.BLINDNESS, 200, 0)));
-
-    public static final RegistryObject<Potion> BLINDNESS_POTION_2 = POTIONS.register("blindness_potion_2",
-            () -> new Potion(new MobEffectInstance(MobEffects.BLINDNESS, 200, 1)));
+    public static final RegistryObject<Item> BLINDNESS_POTION = ITEMS.register("blindness_potion",
+            () -> new AUSplashPotion(new Potion(new MobEffectInstance(MobEffects.BLINDNESS, 200, 0)),
+                    new Item.Properties().stacksTo(1), .5f));
 
     public static final RegistryObject<MobEffect> CUSTOM_POTION_EFFECT = EFFECTS.register("custom_potion_effect",
             AUPotionEffect::new);
@@ -36,29 +35,33 @@ public class ModPotions {
             () -> new AUPotion(new Potion(new MobEffectInstance(MobEffects.HEALTH_BOOST, 2000, 2)),
                     new Item.Properties().stacksTo(1),32));
 
-    public static final RegistryObject<Item> RECALL_POTION
-            = ITEMS.register("recall_potion",
-            () -> new AUPotion(new Potion(new MobEffectInstance(AUEffects.RECALL.get(), 0, 0)), new Item.Properties().stacksTo(1)));
+    public static final RegistryObject<Item> RECALL_POTION = ITEMS.register("recall_potion",
+            () -> new AUPotion(new Potion(new MobEffectInstance(AUEffects.RECALL.get(), 0, 0)),
+                    new Item.Properties().stacksTo(1)));
 
-    public static final RegistryObject<Potion> ANCHORED_RECALL_POTION =
-            POTIONS.register("anchored_recall_potion",
-                    () -> new Potion(new MobEffectInstance(AUEffects.ANCHORED_RECALL.get(), 0, 0)));
+    public static final RegistryObject<Item> ANCHORED_RECALL_POTION = ITEMS.register("anchored_recall_potion",
+            () -> new AUPotion(new Potion(new MobEffectInstance(AUEffects.ANCHORED_RECALL.get(), 0, 0)),
+                    new Item.Properties().stacksTo(1)));
 
-    public static final RegistryObject<Potion> BEGINNING_POTION =
-            POTIONS.register("beginning_potion",
-                    () -> new Potion(new MobEffectInstance(AUEffects.END_RECALL.get(), 0, 0)));
+    public static final RegistryObject<Item> BEGINNING_POTION = ITEMS.register("beginning_potion",
+            () -> new AUPotion(new Potion(new MobEffectInstance(AUEffects.END_RECALL.get(), 0, 0)),
+                    new Item.Properties().stacksTo(1)));
 
-    public static final RegistryObject<Potion> GRAVITY_POTION = POTIONS.register("gravity_potion",
-            () -> new Potion(new MobEffectInstance(AUEffects.GRAVITY.get(), 200, 0)));
+    public static final RegistryObject<Item> GRAVITY_POTION = ITEMS.register("gravity_potion",
+            () -> new AUPotion(new Potion(new MobEffectInstance(AUEffects.GRAVITY.get(), 200, 0)),
+                    new Item.Properties().stacksTo(1)));
 
-    public static final RegistryObject<Potion> THUNDEROUS_POTION = POTIONS.register("thunderous_potion",
-            () -> new Potion(new MobEffectInstance(AUEffects.THUNDEROUS.get(), 1)));
+    public static final RegistryObject<Item> THUNDEROUS_POTION = ITEMS.register("thunderous_potion",
+            () -> new AUSplashPotion(new Potion(new MobEffectInstance(AUEffects.THUNDEROUS.get(), 1)),
+                    new Item.Properties().stacksTo(1), .5f));
 
-    public static final RegistryObject<Potion> EXPLOSIVE_POTION = POTIONS.register("explosive_potion",
-            ()-> new Potion(new MobEffectInstance(AUEffects.EXPLOSIVE.get(), 1, 2)));
+    public static final RegistryObject<Item> EXPLOSIVE_POTION = ITEMS.register("explosive_potion",
+            () -> new AUSplashPotion(new Potion(new MobEffectInstance(AUEffects.EXPLOSIVE.get(), 1, 2)),
+                    new Item.Properties().stacksTo(1), .5f));
 
-    public static final RegistryObject<Potion> FATAL_POISON_POTION = POTIONS.register("fatal_poison_potion",
-            () -> new Potion(new MobEffectInstance(AUEffects.FATAL_POISON.get(), 400)));
+    public static final RegistryObject<Item> FATAL_POISON_POTION = ITEMS.register("fatal_poison_potion",
+            () -> new AUSplashPotion(new Potion(new MobEffectInstance(AUEffects.FATAL_POISON.get(), 400)),
+                    new Item.Properties().stacksTo(1), .5f));
 
     public static void register(IEventBus eventBus) {
         POTIONS.register(eventBus);
